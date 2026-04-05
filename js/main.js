a1a033a8f58a97db125c8145dbdb4891a3e49746
Ads4331x <karkitan@gmail.com>
2026-04-05 16:53:10 +0545

    completed home page v1

--- js/main.js
+++ js/main.js
@@ -6,6 +6,7 @@ function initSlider() {
   let cur = 0,
     timer;
 
+  // go to slide n
   function go(n) {
     slides[cur].classList.remove("active");
     dots[cur] && dots[cur].classList.remove("active");
@@ -14,16 +15,21 @@ function initSlider() {
     dots[cur] && dots[cur].classList.add("active");
   }
 
+  // auto go to next slide every 4.5s
   function start() {
     timer = setInterval(() => go(cur + 1), 4500);
   }
+
+  // reset timer when user clicks prev/next/dot
   function reset() {
     clearInterval(timer);
     start();
   }
 
+  // initialization
   go(0);
   start();
+  // event listeners for prev/next buttons and dots
   document.querySelector(".s-btn.prev")?.addEventListener("click", () => {
     go(cur - 1);
     reset();
@@ -32,6 +38,7 @@ function initSlider() {
     go(cur + 1);
     reset();
   });
+  // add click event to dots
   dots.forEach((d, i) =>
     d.addEventListener("click", () => {
       go(i);
